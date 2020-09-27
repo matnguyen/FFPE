3d7a219d55d34a7538753ea837ca42f86f7f3e6d
Matthew Nguyen <matthewnguyen.667@gmail.com>
2020-09-27 00:25:05 -0700

    Add HTMCP comp resources data

--- cr.R
+++ cr.R
@@ -2,5 +2,5 @@ library(ggraph)
 library(ggplot2)
 
 a <- ggplot(ComputationalResources, aes(fill=Metric, y=log(Value), x=Tool)) + 
-  geom_bar(position="dodge", stat="identity") + ggtitle("Computational Resources BLGSP")
+  geom_bar(position="dodge", stat="identity") + ggtitle("Computational Resources HTMCP")
 plot(a)
